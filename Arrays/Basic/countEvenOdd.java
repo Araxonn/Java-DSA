9148001f6c6923dc2b184327e0a041c38cbd00f5
Lokesh <luckypatil577r@gmail.com>
2026-05-17 23:38:25 +0530

    add countEvenOdd class to count even and odd numbers in an array

---
+++ Arrays/Basic/countEvenOdd.java
@@ -0,0 +1,28 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class countEvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("length of array : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements : ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int even = 0;
+        int odd = 0;
+        for (int i : arr) {
+            if(i%2==0) {
+                even++;
+            } else{
+                odd++;
+            }
+        }
+        System.out.println("Even : "+even);
+        System.out.println("Odd : "+odd);
+
+    }
+}
